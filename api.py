92a32c97419a4c5e9ed95082f6027363461a44db
ryan-ohalloran <79421083+ryan-ohalloran@users.noreply.github.com>
2023-12-30 13:31:20 -0600

    fix bad reponse type parameter

--- api.py
+++ api.py
@@ -16,7 +16,7 @@ class Client(OpenAI):
         )
         return transcript
     
-    def summarize(self, transcript: str, prompt_instructions: List[str], model: str = "gpt-4", temperature: float = 0.9) -> str:#ChatCompletion:
+    def summarize(self, transcript: str, prompt_instructions: List[str], model: str = "gpt-4", temperature: float = 0.9) -> str:
         messages = [
             *({"role": "system", "content": prompt_instruction} for prompt_instruction in prompt_instructions),
             {"role": "system", "content": "Here is the transcript: " + transcript}
@@ -25,7 +25,16 @@ class Client(OpenAI):
             model=model,
             messages=messages,
             temperature=temperature,
-            response_format="text",
         )
-        return response
+        return response.choices[0].message.content
 
+if __name__ == "__main__":
+    print("This is a library and should not be run directly.")
+    from dotenv import load_dotenv
+    import os
+    load_dotenv()
+    OPENAI_KEY = os.getenv("OPENAI_KEY")
+    client = Client(OPENAI_KEY)
+    transcript = client.transcribe(open("Corn.m4a", "rb"))
+    summary = client.summarize(transcript, ["Summarize the audio file."])
+    print(summary)
