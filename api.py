a6bef6fdff34edbc957c462da2c6b0dee7015d6f
ryan-ohalloran <79421083+ryan-ohalloran@users.noreply.github.com>
2023-12-30 13:08:22 -0600

    initial commit

---
+++ api.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+from typing import List
+from openai.types.chat import ChatCompletion
+
+load_dotenv()
+OPENAI_KEY = os.getenv("OPENAI_KEY")
+
+
+class Client(OpenAI):
+    def __init__(self, api_key):
+        super().__init__(api_key=api_key)
+
+    def transcribe(self, audio_file: bytes) -> str:
+        transcript = self.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file,
+            response_format="text",
+        )
+        return transcript
+    
+    def summarize(self, transcript: str, prompt_instructions: List[str], model: str = "gpt-4", temperature: float = 0.9) -> ChatCompletion:
+        messages = [
+            *({"role": "system", "content": prompt_instruction} for prompt_instruction in prompt_instructions),
+            {"role": "system", "content": "Here is the transcript: " + transcript}
+        ]
+        response = self.chat.completions.create(
+            model=model,
+            messages=messages,
+            temperature=temperature,
+            response_format="text",
+        )
+        return response
+
---
+++ app.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import streamlit as st
+from api import Client
+
+# retrieve API key from streamlit secret
+OPENAI_KEY = st.secrets["OPENAI_KEY"]
+
+# instantiate client
+client = Client(OPENAI_KEY)
+
+st.title("Rundown")
+st.write("A tool for summarizing audio recordings.")
+
+st.header("Upload audio file")
+audio_file = st.file_uploader("Upload audio file", type=["wav", "mp3", "mp4", "m4a", "mpg", "mpga", "webm"])
+
+# transcribe audio file
+st.write("Transcribe")
+if audio_file is not None:
+    transcript = client.transcribe(audio_file)
+    st.write("Transcript: " + transcript)
+
+# get prompt instructions as list of text inputs
+st.header("Prompt instructions")
+
+# Use a session state to store the number of text boxes
+if 'num_boxes' not in st.session_state:
+    st.session_state['num_boxes'] = 1
+
+# Create the text boxes
+prompt_instructions = []
+for i in range(st.session_state['num_boxes']):
+    prompt_instructions.append(st.text_input(f"Prompt instruction {i+1}", key=f"Prompt instruction {i+1}"))
+
+# Button to add more text boxes
+if st.button('Add another instruction'):
+    st.session_state['num_boxes'] += 1
+
+# specify model as a text entry
+st.header("Model")
+model = st.text_input("Model", value="gpt-4")
+
+# specify temperature as a slider
+st.header("Temperature")
+temperature = st.slider("Temperature", min_value=0.0, max_value=1.0, value=0.9, step=0.01)
+
+# summarize audio file
+st.header("Summarize audio file")
+summary = client.summarize(transcript, prompt_instructions, model=model, temperature=temperature)
+st.write("Summary: " + summary)
